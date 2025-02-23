9f06207c1a0212fa140aae36fa0e6bd3d3ee344c
Jakub Rezler <jakub.rezler96@gmail.com>
2025-02-23 23:05:29 +0100

    initiate landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./app.scss";
 import BgLanding from "./bgLanding/BgLanding.tsx";
 import NavBarView from "./navBar/NavBarView.tsx";
+import Landing from "./sections/Landing.tsx";
 
 function App() {
   return (
     <div className="w-full flex flex-col items-center sm:px-10 px-2">
       <NavBarView />
       <BgLanding />
+      <div className="mt-[180px] z-1">
+        <Landing />
+      </div>
     </div>
   );
 }
--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 
-import earthImg from "../img/earth.png";
-
-const LandingPage = () => {
-  return <></>;
+const Landing = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2">
+        {/* <h1 className="atkinson text-5xl w-3/4">
+          Landing page i strony internetowe, które zwiększają konwersję
+          <br /> i przyciągną klientów.
+        </h1> */}
+      </div>
+    </div>
+  );
 };
 
-export default LandingPage;
+export default Landing;
